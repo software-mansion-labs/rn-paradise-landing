d0b84f5ba04df259c3598657fd52fa87a4aa5541
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2025-12-02 14:15:06 +0100

    feat: init astro project

---
+++ README.md
@@ -0,0 +1,1 @@
+# rn-paradise-landing
---
+++ src/components/ContentWrapper.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface ContentWrapperProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const ContentWrapper = React.forwardRef<HTMLDivElement, ContentWrapperProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "mx-auto flex w-full max-w-[552px] items-center justify-center max-sm:px-8 sm:w-[552px] sm:max-w-[552px] md:w-[936px] md:max-w-[936px] lg:w-[1360px] lg:max-w-[1360px]",
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+ContentWrapper.displayName = "ContentWrapper";
+
+export default ContentWrapper;
