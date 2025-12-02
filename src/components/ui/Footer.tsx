f6f8f2e79dffec015ff9354b22701f7aee89cafb
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2025-12-02 16:01:45 +0100

    feat: add Header and Footer

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,116 @@
+import ContentWrapper from "@/components/ContentWrapper";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
+
+export interface FooterProps extends React.HTMLAttributes<HTMLElement> {}
+
+const socials = [
+  {
+    icon: "twitter",
+    href: "https://twitter.com/swmansion",
+  },
+  {
+    icon: "facebook",
+    href: "https://www.facebook.com/SoftwareMansion/",
+  },
+  {
+    icon: "github",
+    href: "https://github.com/software-mansion",
+  },
+  {
+    icon: "instagram",
+    href: "https://www.instagram.com/swmansion/",
+  },
+  {
+    icon: "youtube",
+    href: "https://www.youtube.com/c/SoftwareMansion",
+  },
+  {
+    icon: "linkedin",
+    href: "https://www.linkedin.com/company/software-mansion/",
+  },
+  {
+    icon: "dribble",
+    href: "https://dribbble.com/softwaremansion",
+  },
+  {
+    icon: "discord",
+    href: "https://discord.com/invite/2gjSqPQc9Q",
+  },
+];
+
+const Footer = React.forwardRef<HTMLElement, FooterProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <footer
+        id="footer"
+        ref={ref}
+        className={cn(
+          "bg-primary relative bottom-0 w-full overflow-hidden",
+          className,
+        )}
+        {...props}
+      >
+        <ContentWrapper>
+          <div className="mx-auto flex w-full max-w-[1360px] items-center justify-between py-20">
+            <div className="text-primary-foreground flex w-full items-center justify-between gap-8">
+              <div className="flex flex-col items-start gap-2">
+                <h2 className="text-md font-bold">Software Mansion S.A.</h2>
+                <h3 className="text-sm font-normal">
+                  ul. Zabłocie 43b
+                  <br />
+                  30-701 Kraków, Poland
+                  <br />
+                  NIP/VAT EU: PL6793131302
+                </h3>
+              </div>
+
+              <div className="flex flex-col items-center gap-14">
+                <div className="flex items-center gap-1">
+                  {socials.map(({ icon, href }) => (
+                    <a
+                      key={href}
+                      href={href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex h-6 w-6 transform items-center justify-center rounded-full transition-all duration-300 ease-in-out hover:scale-120 hover:opacity-60 md:h-9 md:w-9"
+                    >
+                      <img
+                        src={`/assets/${icon}.svg`}
+                        className="h-3 w-3 md:h-6 md:w-6"
+                        alt={`${icon} icon`}
+                        width="16"
+                        height="16"
+                      />
+                    </a>
+                  ))}
+                </div>
+                <p className="text-sm font-normal">
+                  &copy; Software Mansion 2025.
+                </p>
+              </div>
+
+              <a
+                href="https://swmansion.com/"
+                target="_blank"
+                rel="noopener"
+                className="flex-shrink-0"
+              >
+                <img
+                  src="/assets/logo-swm-top-left.svg"
+                  alt="Software Mansion"
+                  className="h-28 w-48"
+                />
+              </a>
+            </div>
+          </div>
+        </ContentWrapper>
+      </footer>
+    );
+  },
+);
+
+Footer.displayName = "Footer";
+
+export { Footer };
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState, useRef, useImperativeHandle } from "react";
+import { cn } from "@/lib/utils";
+export interface HeaderProps extends React.HTMLAttributes<HTMLElement> {}
+
+const LATEST_NEWS_ID = "v0.5.0";
+
+const navItems = [
+  { name: "About", href: "#about" },
+  { name: "Workshops", href: "#workshops" },
+  { name: "Venue", href: "#venue" },
+  { name: "Tickets", href: "#tickets" },
+  { name: "Previous editions", href: "#previous-editions" },
+  { name: "FAQ", href: "#faq" },
+];
+
+const Header = React.forwardRef<HTMLElement, HeaderProps>(
+  ({ className, ...props }, ref) => {
+    const [isScrolled, setIsScrolled] = useState(true);
+    const [activeTheme, setActiveTheme] = useState("light");
+
+    const localHeaderRef = useRef<HTMLElement>(null);
+    useImperativeHandle(ref, () => localHeaderRef.current!);
+
+    // useEffect(() => {
+    //   const handleScroll = () => {
+    //     const scrollY = window.scrollY;
+    //     setIsScrolled(scrollY > 10);
+
+    //     const headerHeight = localHeaderRef.current?.offsetHeight || 0;
+    //     const triggerPoint = scrollY + headerHeight / 2;
+
+    //     let currentTheme = "dark";
+    //     for (let i = sectionThemes.length - 1; i >= 0; i--) {
+    //       const section = sectionThemes[i];
+    //       const element = document.getElementById(section.id);
+
+    //       if (element) {
+    //         if (triggerPoint >= element.offsetTop) {
+    //           currentTheme = section.theme;
+    //           break;
+    //         }
+    //       }
+    //     }
+    //     setActiveTheme(currentTheme);
+    //   };
+
+    //   handleScroll();
+    //   window.addEventListener("scroll", handleScroll);
+
+    //   return () => {
+    //     window.removeEventListener("scroll", handleScroll);
+    //   };
+    // }, []);
+
+    return (
+      <header
+        ref={localHeaderRef}
+        className={cn(
+          "sticky top-0 z-50 w-full transition-all duration-300 ease-in-out",
+          activeTheme === "dark"
+            ? "bg-primary text-primary-foreground"
+            : "text-primary bg-white",
+          isScrolled
+            ? activeTheme === "dark"
+              ? "border-b border-white/40"
+              : "border-b border-black/10"
+            : "border-b border-transparent",
+          className,
+        )}
+        {...props}
+      >
+        <div className="mx-auto flex h-20 w-full max-w-[1360px] items-center justify-between px-7 sm:px-8">
+          <a href="#hero" className="flex items-center gap-2 pt-15">
+            <img
+              src="/assets/logo-swm-white.svg"
+              alt="Software Mansion"
+              className="h-16 w-32"
+            />
+          </a>
+
+          <nav className="mx-auto hidden items-center justify-center gap-10 md:flex">
+            {navItems.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className={cn(
+                  "font-aeonik text-md justify-center font-normal",
+                  "transition-all hover:scale-105",
+                  activeTheme === "dark"
+                    ? "hover:text-slate-300"
+                    : "hover:text-primary/70",
+                  "last:hidden",
+                  "lg:last:block",
+                )}
+                onClick={() => {
+                  window.location.href = item.href;
+                }}
+              >
+                <div className="flex items-center justify-center gap-1">
+                  {item.name}
+                </div>
+              </a>
+            ))}
+          </nav>
+
+          <div className="flex items-center gap-5 sm:gap-10">
+            <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+              className={cn(
+                "font-aeonik text-md font-normal underline underline-offset-2",
+                activeTheme === "dark"
+                  ? "hover:text-slate-300"
+                  : "hover:text-primary/70",
+              )}
+            >
+              Buy the ticket
+            </a>
+          </div>
+        </div>
+      </header>
+    );
+  },
+);
+
+Header.displayName = "Header";
+
+export { Header };
