c5a65e060438dbb8eecf93f6ed029517be156722
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2025-12-02 18:10:29 +0100

    feat: add faq section

---
+++ src/components/ui/AccordionWrapper.tsx
@@ -0,0 +1,41 @@
+import {
+  Accordion as BaseAccordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface RoadmapAccordionProps {
+  data: {
+    question: string;
+    answer: string;
+  }[];
+}
+
+export function Accordion({ data }: RoadmapAccordionProps) {
+  return (
+    <BaseAccordion
+      type="single"
+      collapsible
+      defaultValue={`item-0`}
+      className="w-full"
+    >
+      {data.map((item, i) => {
+        return (
+          <AccordionItem value={`item-${i.toFixed()}`} key={item.question}>
+            <AccordionTrigger>
+              <p className="text-primary text-left text-xl font-medium">
+                {item.question}
+              </p>
+            </AccordionTrigger>
+            <AccordionContent>
+              <p className="text-primary text-md text-left font-medium">
+                {item.answer}
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        );
+      })}
+    </BaseAccordion>
+  );
+}
