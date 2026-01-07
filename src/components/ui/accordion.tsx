eba5b6ae99d5f89a60c4ce4fce6127c5eff362e2
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2026-01-07 13:15:44 +0100

    fix: change border style in accordion

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -17,7 +17,7 @@ function AccordionItem({
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-primary border-b last:border-b-0", className)}
+      className={cn("border-gray-border border-b last:border-b-0", className)}
       {...props}
     />
   );
@@ -39,7 +39,7 @@ function AccordionTrigger({
         {...props}
       >
         {children}
-        <ChevronDownIcon className="text-primary pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        <ChevronDownIcon className="text-primary pointer-events-none size-6 shrink-0 translate-y-0.5 transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   );
