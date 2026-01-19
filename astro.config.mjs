f30efe5f509d781c7c7e89f7701d0a4d62c3e24c
Paweł Małecki <92953623+p-malecki@users.noreply.github.com>
2026-01-19 13:19:03 +0100

    chore: add build and publish workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",
   integrations: [react()],
 
   vite: {
